b39ba3c7afd6d0b94525e714a5ddd48b53474c79
David Osorio <osoriodev@outlook.com>
2021-12-26 18:43:28 -0600

    Add error icon independently

--- js/script.js
+++ js/script.js
@@ -3,18 +3,22 @@ const logup = document.querySelector('.logup')
 const logupElements = [
   {
     field: document.getElementById('firstname'),
+    icon: document.getElementById('firstname-icon'),
     message: document.getElementById('firstname-message')
   },
   {
     field: document.getElementById('lastname'),
+    icon: document.getElementById('lastname-icon'),
     message: document.getElementById('lastname-message')
   },
   {
     field: document.getElementById('email'),
+    icon: document.getElementById('email-icon'),
     message: document.getElementById('email-message')
   },
   {
     field: document.getElementById('password'),
+    icon: document.getElementById('password-icon'),
     message: document.getElementById('password-message')
   }
 ]
@@ -32,13 +36,15 @@ const inputs = {
 
 const displayError = element => {
   element.field.classList.add('logup__input--warning')
-  element.message.classList.add('logup__error--active')
+  element.icon.classList.add('logup__icon--warning')
+  element.message.classList.add('logup__message--warning')
   inputs[element.field.name] = false
 }
 
 const hideError = element => {
   element.field.classList.remove('logup__input--warning')
-  element.message.classList.remove('logup__error--active')
+  element.icon.classList.remove('logup__icon--warning')
+  element.message.classList.remove('logup__message--warning')
   inputs[element.field.name] = true
 }
 
@@ -46,13 +52,7 @@ const isAnEmail = email => re.test(email)
 
 const validateForm = () => {
   logupElements.forEach(element => {
-    if (!(element.field.name === 'email')) {
-      if (element.field.value === '') {
-        displayError(element)
-      } else {
-        hideError(element)
-      }
-    } else {
+    if (element.field.name === 'email') {
       if (element.field.value === '') {
         element.message.innerText = 'Email cannot be empty'
         displayError(element)
@@ -62,6 +62,9 @@ const validateForm = () => {
       } else {
         hideError(element)
       }
+    } else {
+      if (element.field.value === '') displayError(element)
+      else hideError(element)
     }
   })
 
